ebbc6562a16e22491f5505e93b545af40c158b1e
yqk666 <1615964588@qq.com>
2020-03-16 12:00:31 +0800

    yu

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+
+         new Thread(new Runnable(){
+             public void run() {
+                 System.out.println();
+                 System.out.println("-----------");
+             }
+         }).start();
+    }
+}
